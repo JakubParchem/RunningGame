c84ef6679d9f1de7c242992e2f8ed455c961d833
JakubParchem <jakubparchem@gmail.com>
2024-01-10 15:51:08 +0100

    added library to the project directory

--- game_window.cpp
+++ game_window.cpp
@@ -1,3 +1,4 @@
+#include<ncursesw/ncurses.h>
 #include "Player.cpp"
 #include "Cacti.cpp"
 class Game
@@ -6,38 +7,40 @@ class Game
 	Cactus cactus[2];
 	int height,width,starty,startx;
 	int speed;
-	int space;
-	int delay;
-	bool delay_active;
-	bool lost;
+	int space=24;
+	int delay=0;
+	bool delay_active=false;
 	WINDOW *win;
 	public:
 		int score=0;
 		int highscore=0;
 		void start()
 		{
+			cactus[0].active=true;
+			score=0;
+			speed=1;
+			space=24;
+			delay=0;
+			int next_space=0;
 			window_setup();
 			int i=0;
+			bool lost=false;
 			while(!lost)
 			{
-				printvar();
-				speedup();
-				if(delay_active)
-				{
-					delay--;
-				}
+				printspeed();
 				jumping(i);
 				if(i=='q')
 				{
 					break;
 				}
-				cacti_moving(speed,space);
+				cacti_moving(speed,space,next_space);
 				for(auto i:cactus)
 				{
-					cactus_collision(lost,i);
+					collision(lost,i);
 				}
 				print_score();
-				score++;	
+				score++;
+				speedup();
 			}
 			game_end();
 		}
@@ -72,11 +75,32 @@ class Game
 		}
 		void window_setup()
 		{
-			set_variables_value();
-			window_initialisation();
-			objects_setup();	
+			initscr();
+			keypad(stdscr, TRUE);
+			curs_set(0);
+			noecho();
+			timeout(28);
+			height = 15;
+			width = 70;
+			get_middle(stdscr,starty,startx,height,width);
+			win =newwin(height,width,starty,startx);
+			refresh();
+			box(win,0,0);
+			wrefresh(win);
+			cactus[0].active=true;
+			cactus[1].active=false;
+			for(auto i:cactus)
+			{
+				i.clean(win);
+			}
+			player.clean(win);
+			cactus[0].rand_sprite();
+			cactus[1].rand_sprite(highscore);
+			draw_ground();
+			player.prin(win);
+			cactus[0].prin(win);
 		}
-		void cactus_collision(bool &lost,Cactus &cactus)
+		void collision(bool &lost,Cactus &cactus)
 		{
 			if(player.posx==cactus.posx and player.posy>=9-cactus.height)
 				{
@@ -86,18 +110,33 @@ class Game
 		void speedup()
 		{
 			if(speed==1 and (score>=400 and (cactus[0].posx-player.posx)%2==0))
-			{
-				set_delay();
-			}
-			else if(speed==2 and (score>=1250 and (cactus[0].posx-player.posx)%3==0))
-			{
-				set_delay();
-			};	
-			if(delay==0 and delay_active)
-			{
-				speed++;
-				delay_active=false;
-			}			
+				{
+					set_delay();
+					if(delay==0)
+					{
+						speed=2;
+						delay_active=false;
+					}
+				}
+				if(speed==2 and (score>=1250 and (cactus[0].posx-player.posx)%3==0))
+				{
+					set_delay();
+					if(delay==0)
+					{
+						speed=3;
+						delay_active=false;
+					}
+				};
+				if(delay_active)
+				{
+					delay--;
+				}			
+		}
+		void printspeed()
+		{
+			mvwprintw(win,1,1,"                    ");
+			mvwprintw(win,1,1,"speed : %i",delay);
+			wrefresh(win);
 		}
 		void jumping(int &i)
 		{
@@ -108,22 +147,33 @@ class Game
 			}			
 			player.jump(win);
 		}
-		void cacti_moving(int &speed, int &space)
+		void game_end()
 		{
-				if(!cactus[1].active and cactus[0].posx<=66-space and !delay_active)
+			high_score();
+			clear();
+			cactus[0].rand_sprite();
+			cactus[1].rand_sprite(highscore);
+			delwin(win);
+			endwin();
+		}
+		void cacti_moving(int &speed, int &space, int &next_space)
+		{
+				if(!cactus[1].active and cactus[0].posx<=66-space)
 				{
 						cactus[1].rand_space(space);
 						cactus[1].active=true;
 				}
-				if(!delay_active)
+				if(!cactus[0].active and delay==0)
 				{
 					cactus[0].active=true;
 				}
-				cactus[0].cmove(win,speed,true);
-				if(cactus[1].active)
+				for(auto i:cactus)
 				{
-					cactus[1].cmove(win,speed,true);
-				}
+					if(i.active)
+					{
+						i.cmove(win,speed,true);
+					}	
+				}				
 		}
 		void set_delay()
 		{
@@ -132,78 +182,6 @@ class Game
 				delay=66;
 				delay_active=true;
 			}
-		}
-		
-		//window actions
-		
-		void set_variables_value()
-		{
-			height = 15;
-			width = 70;
-			cactus[0].active=true;
-			cactus[1].active=false;
-			score=0;
-			speed=1;
-			space=24;
-			delay=0;
-			delay_active=false;
-			lost=false;
-		}
-		
-		void window_initialisation()
-		{
-			initscr();
-			keypad(stdscr, TRUE);
-			curs_set(0);
-			noecho();
-			timeout(28);
-			get_middle(stdscr,starty,startx,height,width);
-			win =newwin(height,width,starty,startx);
-			refresh();
-			box(win,0,0);
-			wrefresh(win);
-			draw_ground();
-		}
-		
-		void objects_setup()
-		{
-			for(auto i:cactus)
-			{
-				i.clean(win);
-			}
-			player.clean(win);
-			cactus[0].rand_sprite();
-			cactus[1].rand_sprite(highscore);
-			player.prin(win);
-			cactus[0].prin(win);
-		}
-		
-		void printvar()
-		{
-			mvwprintw(win,1,1,"                    ");
-			mvwprintw(win,1,1,"speed : %i",delay);
-			wrefresh(win);
-		}
-		
-		void game_end()
-		{
-			high_score();
-			wclear(win);
-			print_GameOver();
-			cactus[0].rand_sprite();
-			cactus[1].rand_sprite(highscore);
-			delwin(win);
-			endwin();
-		}
-		void print_GameOver()
-		{
-			mvwprintw(win,6,30,"Game Over!");
-			mvwprintw(win,7,21,"press any button to continue");
-			wrefresh(win);
-			timeout(10000);
-			getch();
-			timeout(28);
-			clear();
 		}
 	
 };
